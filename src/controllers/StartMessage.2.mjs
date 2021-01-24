ed3a1fbb95d10b46f6479919a944817783959b02
Bert Bruynooghe <bert@bruynooghe-polet.com>
2021-01-24 11:12:54 +0100

    all StartMessage interaction with Stimulus

--- src/controllers/StartMessage.2.mjs
+++ src/controllers/StartMessage.2.mjs
@@ -2,14 +2,14 @@ export default Controller => {
   class StartMessageController extends Controller {
     connect () {
       if (0 <= document.cookie.indexOf('dontShowIntro=true')) return
-      document.getElementById('modal-control').setAttribute('checked', '')
+      this.modalVisibilityCheckBoxTarget.setAttribute('checked', '')
     }
     close () {
       document.cookie = `dontShowIntro=${this.dontShowAnymoreCheckBoxTarget.checked}`
       return true
     }
   } 
-  StartMessageController.targets = ['dontShowAnymoreCheckBox']
+  StartMessageController.targets = ['dontShowAnymoreCheckBox', 'modalVisibilityCheckBox']
 
   return StartMessageController
 }
